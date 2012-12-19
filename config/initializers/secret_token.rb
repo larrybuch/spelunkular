decfbe9ff7ed865ccc672fef731b3aa0c3d3ead0
Larry Buchanan <larrybuch@gmail.com>
2012-12-19 14:29:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spelunkular::Application.config.secret_token = 'f4a49e3ba15142c89cded302ef219fa3cde1dfadad8de0550f6f626fbf0715f1cbf1ce2ce2e9094c9d257be6c4dcc073f7f134fc3b3e04091b5c38d3ff55d002'
